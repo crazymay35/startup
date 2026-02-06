370636c187d149ceeb0e74a0e1029c189e848df3
crazymay35 <89706779+crazymay35@users.noreply.github.com>
2026-02-06 00:26:44 -0700

    tweeked following page, changed unfollow button color, made notification symbols + or x for add or remove (looks better on phone)

--- src/following/following.jsx
+++ src/following/following.jsx
@@ -9,15 +9,15 @@ export function Following() {
             <span id="following-faculty-glyphic-regular">FRIENDS</span>
             <div>
                 USERNAME1
-                <button type="button" className="btn btn-primary my-button">Unfollow</button>
+                <button type="button" className="btn btn-secondary">Unfollow</button>
             </div>
             <div>
                 USERNAME2
-                <button type="button" className="btn btn-primary my-button">Unfollow</button>
+                <button type="button" className="btn btn-secondary">Unfollow</button>
             </div>
             <div>
                 USERNAME3
-                <button type="button" className="btn btn-primary my-button">Unfollow</button>
+                <button type="button" className="btn btn-secondary">Unfollow</button>
             </div>
             <form className="form-thing">
                 <input type="email" className="form-control" placeholder="example@email.com"/>
@@ -28,18 +28,18 @@ export function Following() {
             <span id="following-faculty-glyphic-regular">NOTIFICATIONS</span>
             <div>
                 USERNAME1 shared a palette
-                <button type="button" className="btn btn-primary my-button">Add</button>
-                <button type="submit" className="btn btn-secondary">X</button>
+                <button type="button" className="btn btn-primary my-button">+</button>
+                <button type="submit" className="btn btn-secondary">x</button>
             </div>
             <div>
                 USERNAME1 shared a palette
-                <button type="button" className="btn btn-primary my-button">Add</button>
-                <button type="submit" className="btn btn-secondary">X</button>
+                <button type="button" className="btn btn-primary my-button">+</button>
+                <button type="submit" className="btn btn-secondary">x</button>
             </div>
             <div>
                 USERNAME3 shared a palette
-                <button type="button" className="btn btn-primary my-button">Add</button>
-                <button type="submit" className="btn btn-secondary">X</button>
+                <button type="button" className="btn btn-primary my-button">+</button>
+                <button type="submit" className="btn btn-secondary">x</button>
             </div>
         </div>
     </main>

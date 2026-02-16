418d69ba1dee3ca191c00bfef881893d90c9170d
crazymay35 <89706779+crazymay35@users.noreply.github.com>
2026-02-16 00:03:06 -0700

    create account now working, gotta figure out what I'm gonna do for the other pages (if i'm going to have them display the links at all or no etc)

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -12,8 +12,6 @@ export function Login() {
     const[showCreateAccount,setShowCreateAccount] = useState(false);
 
     const navigate = useNavigate();
-
-    localStorage.setItem("alex123@gmail.com", JSON.stringify({ email: "alex123@gmail.com", username: "Alex", password: "pass123" }) );
  
     function handleLogin(e) {
         e.preventDefault();
@@ -46,7 +44,10 @@ export function Login() {
     }
     function handleCreate(e) {
         e.preventDefault();
-        //console.log("Creating account: ", username, email, password)
+        if (!username) {
+            console.log("username required")
+            return;
+        }
         if (!email) {
             console.log("email required")
             return;
@@ -64,22 +65,24 @@ export function Login() {
   return (
     <main>
         {showCreateAccount && 
-        <div className='login-modal'>
-            <h3 id="login-faculty-glyphic-regular">CREATE</h3>
-            <h3 id="login-faculty-glyphic-regular" className='login-thing'>ACCOUNT</h3>
-            <form onSubmit={handleCreate}>
-                <input type="username" className="form-control" placeholder="username"
-                    value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <input type="email" className="form-control" placeholder="example@email.com" 
-                    value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <input type="password" className="form-control" placeholder="password"
-                    value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <span>
-                    <button type="createAccount" className="btn btn-secondary">Create</button>
-                </span>
-            </form>
-        </div>
-        };
+            <div className='login-modal'>
+                <h3 id="login-faculty-glyphic-regular">CREATE</h3>
+                <h3 id="login-faculty-glyphic-regular" style={{marginBottom: "1rem"}}>ACCOUNT</h3>
+                <form onSubmit={handleCreate}>
+                    <input type="text" className="form-control" placeholder="username"
+                        value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    <input type="email" className="form-control" placeholder="example@email.com" 
+                        value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" className="form-control" placeholder="password"
+                        value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <span>
+                        <button type="createAccount" className="btn btn-primary my-button">Create</button>
+                        <button className="btn btn-secondary" 
+                        onClick={() => setShowCreateAccount(false)}>Cancel</button>
+                    </span>
+                </form>
+            </div>
+        }
         <div className="login-main-transparent-container">
             <h3 id="login-faculty-glyphic-regular">WELCOME TO</h3>
             <h2>COLOR PAL!</h2>            

f1ece497de863dbb85e50d1c3608c2f8c7fd4b73
crazymay35 <89706779+crazymay35@users.noreply.github.com>
2026-03-17 23:13:36 -0600

    make sure config is local host

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4000', // Changed 'localhost' to '127.0.0.1'
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:4000',
     },
   },
 });
